62408fa5902bd13551262a77bb69a233c4c3f9d9
yudantoardi <yudantoardi@gmail.com>
2021-09-09 17:11:00 +0700

    update html all pages

---
+++ asset/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    $(".src-toggle-bt").click(function(e){
+        $(".src-form").addClass("active");
+        $(".src-form input").focus();
+    });
+
+    $(".src-form").click(function(e){
+        e.stopPropagation();
+    });
+
+    $(document).click(function(){
+        $(".src-form").removeClass("active");
+    });
+
+    $(".tab-bt li a").click(function(t){
+        t.preventDefault();
+        $(".tab-ct").removeClass("show");
+        $($(this).attr("href")).addClass("show");
+
+        $(".tab-bt li").removeClass("active");
+        $($(this).parent()).addClass("active");
+    });
+
+});
